f9e5c8f88151325f90ba70ea57e9e407e1598769
Caspar Gross <mail@caspar.one>
2025-04-14 13:39:05 +0200

    Create QC tables

---
+++ analysis_other/repeatmasker/repeatmasker.sh
@@ -0,0 +1,8 @@
+RepeatMasker \
+    -species human \
+    -lib /mnt/storage3b/projects/no_ngsd/ahthapp1_T2T_ONT/data/ref/dfam39_full.7.h5.gz \
+    -dir analysis_other\repeat_masker \
+    -pa 16 \
+    assembly/output/verkko/TUE_02_03UL/assembly.fasta
+
+
