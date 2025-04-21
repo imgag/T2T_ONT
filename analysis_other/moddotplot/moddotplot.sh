940417faf5ef695a75f8a88e46d53a6a8e8d12d1
Caspar Gross <post@caspar.bio>
2025-04-21 23:11:39 +0200

    Verkko trio phasing

--- analysis_other/moddotplot/moddotplot.sh
+++ analysis_other/moddotplot/moddotplot.sh
@@ -1,3 +1,8 @@
+# Reference dataset must be copied into the environment
+
+
 HP1="assembly/output/verkko/TUE_02_03UL/assembly.haplotype1.fasta"
 HP2="assembly/output/verkko/TUE_02_03UL/assembly.haplotype2.fasta"
 
+moddotplot interactive -f analysis_other/moddotplot/TUE_02_03UL.haplotype1.chr13.fasta
+ssh -N -f -L 8050:127.0.0.1:8050 srv023
--- analysis_other/repeatmasker/repeatmasker.sh
+++ analysis_other/repeatmasker/repeatmasker.sh
@@ -1,8 +1,19 @@
 RepeatMasker \
     -species human \
-    -lib /mnt/storage3b/projects/no_ngsd/ahthapp1_T2T_ONT/data/ref/dfam39_full.7.h5.gz \
-    -dir analysis_other\repeat_masker \
+    -dir analysis_other/repeatmasker/TUE_02_03UL \
     -pa 16 \
+    -gff \
+    -html \
+    assembly/output/verkko/TUE_02_03UL/assembly.fasta
+
+
+RepeatMasker \
+    -species human \
+    -dir analysis_other/repeatmasker/TUE_02_03UL \
+    -pa 16 \
+    -e abblast \
+    -gff \
+    -html \
     assembly/output/verkko/TUE_02_03UL/assembly.fasta
 
 
