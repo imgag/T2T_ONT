b07ca627a3797d0ad6059533dc7bebe4f9180d2c
Caspar Gross <post@caspar.bio>
2025-04-22 18:28:40 +0200

    Put meryl files into subfolder

--- analysis_other/repeatmasker/repeatmasker.sh
+++ analysis_other/repeatmasker/repeatmasker.sh
@@ -1,19 +1,10 @@
-RepeatMasker \
-    -species human \
-    -dir analysis_other/repeatmasker/TUE_02_03UL \
-    -pa 16 \
-    -gff \
-    -html \
-    assembly/output/verkko/TUE_02_03UL/assembly.fasta
-
+mamba activate activate /mnt/storage2/users/ahgrosc1/environments/envs/snakemake/envs/repeatmasker/
 
 RepeatMasker \
     -species human \
     -dir analysis_other/repeatmasker/TUE_02_03UL \
-    -pa 16 \
-    -e abblast \
+    -pa 64 \
     -gff \
     -html \
     assembly/output/verkko/TUE_02_03UL/assembly.fasta
-
 
