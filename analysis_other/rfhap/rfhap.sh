6163f6fb1d0262344ec4cc403cc1ce93d9a5d081
Caspar Gross <post@caspar.bio>
2025-04-23 21:37:48 +0200

    Add rfhap execution

---
+++ analysis_other/rfhap/rfhap.sh
@@ -0,0 +1,5 @@
+./bin/nextflow run bin/rfhap-1.0/rfhap.nf \
+    --paternal_reads analysis_other/rfhap/paternal.txt \
+    --maternal_reads analysis_other/rfhap/maternal.txt \
+    --child_reads analysis_other/rfhap/child.txt \
+    --outdir analysis_other/rfhap 
